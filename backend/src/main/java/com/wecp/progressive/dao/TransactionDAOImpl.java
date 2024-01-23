7d69b1bc91ac244a620fe18ef480d89d59179d1c
username <wecp-dummy@gmail.com>
2024-01-23 13:10:05 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/dao/TransactionDAOImpl.java
@@ -0,0 +1,45 @@
+package com.wecp.progressive.dao;
+
+import java.util.List;
+
+import com.wecp.progressive.entity.Transactions;
+
+public class TransactionDAOImpl implements TransactionDAO{
+
+    @Override
+    public int addTransaction(Transactions transaction) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'addTransaction'");
+        return -1;
+    }
+
+    @Override
+    public Transactions getTransactionById(int transactionId) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getTransactionById'");
+        return null;
+    }
+
+    @Override
+    public void updateTransaction(Transactions transaction) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'updateTransaction'");
+        
+    }
+
+    @Override
+    public void deleteTransaction(int transactionId) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'deleteTransaction'");
+    }
+
+    @Override
+    public List<Transactions> getAllTransactions() {
+        // TODO Auto-generated method stub
+       // throw new UnsupportedOperationException("Unimplemented method 'getAllTransactions'");
+       return null;
+    }
+
+
+
+}
